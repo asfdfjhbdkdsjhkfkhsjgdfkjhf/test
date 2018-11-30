1741366c16f0f6ea176c004ca80899a1630accb6
asfdfjhbdkdsjhkfkhsjgdfkjhf <45268803+asfdfjhbdkdsjhkfkhsjgdfkjhf@users.noreply.github.com>
2018-12-01 00:10:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,16 @@
-const Discord = require("discord.js");//@Flasteni
-const falsteni = new Discord.Client();//@Flasteni
-var prefix = "!"
-	falsteni.on("ready", () => {
-let channel =     falsteni.channels.get("515208908415959059")
-setInterval(function() {
-channel.send(`DDDDDDDD`);
-}, 15)
+const Discord = require("discord.js");
+
+const falsteni = new Discord.Client();
+
+
+falsteni.on('ready', async() => {
+var server = "515196917605466128"; 
+var channel = "515208908415959059";
+    setInterval(()=>{
+    falsteni.guilds.get(server).channels.get(channel).send('**SDSSSSSSSS,sddddddddd,sdddddddddddd,dssssssssssss,sdddddddddddd,sddddddddddd **')
+    },305);
 })
+
 falsteni.on('message', message => {
     let prefix = 'v1';
    if(message.content.startsWith('v1say')) {
@@ -22,14 +26,15 @@ falsteni.user.setAvatar(`https://cdn.discordapp.com/attachments/4808845255058391
 });	
 falsteni.login('NTE1MjM4MTMzMDEzNDEzODkw.DtiMxA.q189UHQ9qHZElHRkiAtXQYdco7U');
 
-const falsteni1 = new Discord.Client();//@Flasteni
-var prefix = "!"
+const falsteni1 = new Discord.Client();
 
-	falsteni1.on("ready", () => {
-let channel =     falsteni1.channels.get("515208908415959059")
-setInterval(function() {
-channel.send(`DDDDDDDD`);
-}, 15)
+
+falsteni1.on('ready', async() => {
+var server = "515196917605466128"; 
+var channel = "515208908415959059";
+    setInterval(()=>{
+    falsteni1.guilds.get(server).channels.get(channel).send('**SDSSSSSSSS,sddddddddd,sdddddddddddd,dssssssssssss,sdddddddddddd,sddddddddddd **')
+    },305);
 })
 falsteni1.on('message', message => {
     let prefix = 'v1';
@@ -46,14 +51,16 @@ falsteni1.user.setAvatar(`https://cdn.discordapp.com/attachments/480884525505839
 });	
 falsteni1.login('NTE1MjM4NjU5NjU0NjgwNTk1.DtiNmg.ftL9XFY7jiHyfyfApxMRmIoc-zg');
 
-const falsteni2 = new Discord.Client();//@Flasteni
-var prefix = "!"
 
-	falsteni2.on("ready", () => {
-let channel =     falsteni2.channels.get("515208908415959059")
-setInterval(function() {
-channel.send(`DDDDDDDD`);
-}, 15)
+const falsteni2 = new Discord.Client();
+
+
+falsteni2.on('ready', async() => {
+var server = "515196917605466128"; 
+var channel = "515208908415959059";
+    setInterval(()=>{
+    falsteni2.guilds.get(server).channels.get(channel).send('**SDSSSSSSSS,sddddddddd,sdddddddddddd,dssssssssssss,sdddddddddddd,sddddddddddd **')
+    },305);
 })
 falsteni2.on('message', message => {
     let prefix = 'v1';
@@ -70,14 +77,16 @@ falsteni2.user.setAvatar(`https://cdn.discordapp.com/attachments/480884525505839
 });	
 falsteni2.login('NTE1MjM5NTIwNTk0MTY1Nzgy.DtiOPg.W1rOOBZqls_x3-d-yXX_fZFqlOU');
 
-const falsteni3 = new Discord.Client();//@Flasteni
-var prefix = "!"
+
+const falsteni3 = new Discord.Client();
+
 	
-	falsteni3.on("ready", () => {
-let channel =     falsteni3.channels.get("515208908415959059")
-setInterval(function() {
-channel.send(`DDDDDDDD`);
-}, 15)
+falsteni3.on('ready', async() => {
+var server = "515196917605466128"; 
+var channel = "515208908415959059";
+    setInterval(()=>{
+    falsteni3.guilds.get(server).channels.get(channel).send('**SDSSSSSSSS,sddddddddd,sdddddddddddd,dssssssssssss,sdddddddddddd,sddddddddddd **')
+    },305);
 })
 falsteni3.on('message', message => {
     let prefix = 'v1';
@@ -95,14 +104,17 @@ falsteni3.user.setAvatar(`https://cdn.discordapp.com/attachments/480884525505839
 falsteni3.login('NTE1MjQwMjkwNjAxMjcxMjk2.DtiO8g.Mm8Jh8RGEe4P1ZiZb0Z8Xf4wWSA');
 
 
-const falsteni4 = new Discord.Client();//@Flasteni
-var prefix = "!"
 
-	falsteni4.on("ready", () => {
-let channel =     falsteni4.channels.get("515208908415959059")
-setInterval(function() {
-channel.send(`DDDDDDDD`);
-}, 15)
+const falsteni4 = new Discord.Client();
+
+
+
+falsteni4.on('ready', async() => {
+var server = "515196917605466128"; 
+var channel = "515208908415959059";
+    setInterval(()=>{
+    falsteni4.guilds.get(server).channels.get(channel).send('**SDSSSSSSSS,sddddddddd,sdddddddddddd,dssssssssssss,sdddddddddddd,sddddddddddd **')
+    },305);
 })
 falsteni4.on('message', message => {
     let prefix = 'v1';
@@ -120,14 +132,15 @@ falsteni4.user.setAvatar(`https://cdn.discordapp.com/attachments/480884525505839
 falsteni4.login('NTE1MjQxMDQxNDEwOTE2MzU0.DtiPew.81GEzPwqwcBPdqfk3a-BmsYzDbU');
 
 
-const falsteni5 = new Discord.Client();//@Flasteni
-var prefix = "!"
+const falsteni5 = new Discord.Client();
 
-	falsteni5.on("ready", () => {
-let channel =     falsteni5.channels.get("515208908415959059")
-setInterval(function() {
-channel.send(`DDDDDDDD`);
-}, 15)
+
+falsteni5.on('ready', async() => {
+var server = "515196917605466128"; 
+var channel = "515208908415959059";
+    setInterval(()=>{
+    falsteni5.guilds.get(server).channels.get(channel).send('**SDSSSSSSSS,sddddddddd,sdddddddddddd,dssssssssssss,sdddddddddddd,sddddddddddd **')
+    },305);
 })
 falsteni5.on('message', message => {
     let prefix = 'v1';
@@ -143,3 +156,57 @@ falsteni5.user.setAvatar(`https://cdn.discordapp.com/attachments/480884525505839
 }
 });	
 falsteni5.login('NTE1MjMxNDE5MDkwMzM3Nzk0.DtiHag.0J8Z8ikjjpg_rMRNxtmfkcEpg9Y');
+
+
+const falsteni6 = new Discord.Client();
+
+
+falsteni6.on('ready', async() => {
+var server = "515196917605466128"; 
+var channel = "515208908415959059";
+    setInterval(()=>{
+    falsteni6.guilds.get(server).channels.get(channel).send('**SDSSSSSSSS,sddddddddd,sdddddddddddd,dssssssssssss,sdddddddddddd,sddddddddddd **')
+    },305);
+})
+falsteni6.on('message', message => {
+    let prefix = 'v1';
+   if(message.content.startsWith('v1say')) {
+       let args = message.content.split(' ').slice(1).join(' ');
+       message.channel.send(args)
+   } 
+});
+falsteni6.on('message', message => {
+     if (message.content === "!av-set") {
+falsteni6.user.setAvatar(`https://cdn.discordapp.com/attachments/480884525505839104/515234980612538368/006.jpg`)
+ 
+}
+});	
+falsteni6.login('NTE1MjMyODgzNjc0MzE2ODIw.DtiJ2Q.uwM39e2gEqIa0lBYqaJKmEeC0Ag');
+
+
+
+const falsteni7 = new Discord.Client();
+
+
+	
+falsteni7.on('ready', async() => {
+var server = "515196917605466128"; 
+var channel = "515208908415959059";
+    setInterval(()=>{
+    falsteni7.guilds.get(server).channels.get(channel).send('**SDSSSSSSSS,sddddddddd,sdddddddddddd,dssssssssssss,sdddddddddddd,sddddddddddd **')
+    },305);
+})
+falsteni7.on('message', message => {
+    let prefix = 'v1';
+   if(message.content.startsWith('v1say')) {
+       let args = message.content.split(' ').slice(1).join(' ');
+       message.channel.send(args)
+   } 
+});
+falsteni7.on('message', message => {
+     if (message.content === "!av-set") {
+falsteni7.user.setAvatar(`https://cdn.discordapp.com/attachments/480884525505839104/515234980612538368/006.jpg`)
+ 
+}
+});	
+falsteni7.login('NTE1MjM1NDk5NTU2OTk1MDg1.DtiKjg.cpVWCWEqxXpxcL6nhPr2J2ALKkU');
